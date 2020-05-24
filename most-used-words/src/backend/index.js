188ede7d42981d0f2333215b42c808534622d8f8
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-23 23:22:48 -0300

    funcao junta legendas e retorna array

--- most-used-words/src/backend/index.js
+++ most-used-words/src/backend/index.js
@@ -1,10 +1,15 @@
 const { ipcMain } = require('electron')
+const pathsToRows = require('./pathsToRows')
+
 
 ipcMain.on('process-subtitles', (event, paths) => {
-    console.log(paths)
-    event.reply('process-subtitles', [ 
-        { name: 'you', amount: 258 },
-        { name: 'i', amount: 123 },
-        { name: 'he', amount: 987 }
-    ])
+    pathsToRows(paths)
+        .then(rows)
+        then( () => {
+            event.reply('process-subtitles', [ 
+                { name: 'you', amount: 258 },
+                { name: 'i', amount: 123 },
+                { name: 'he', amount: 987 }
+            ]) 
+        })
 })
